d06a3920a75014c1f5f8983ac615deb44f917f19
zskgit <zskgit@gmail.com>
2021-10-15 16:39:26 +0200

    cdv

--- stac/project1/routes/web.php
+++ stac/project1/routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cdv', function(){
+  return view('cdv', ['name' => 'Janusz', 'surname' => 'Nowak']);
+});
+
+Route::get('/pages/{x}', function ($x) {
+  $pages = [
+    'about' => 'Strona o nas',
+    'contact' => 'jan@gmail.com',
+    'home' => 'Strona domowa'
+  ];
+  return $pages[$x];
+});
+
+Route::get('/address/{city?}/{street?}/{zipCode?}', function(string $city="-", string $street ="brak danych", int $zipCode=null){
+  $zipCode = substr($zipCode, 0, 2)."-".substr($zipCode, 2, 3);
+  echo <<< ADDRESS
+    Kod pocztowy: $zipCode,
+    miasto: $city<br>
+    ulica: $street<hr>
+ADDRESS;
+})->name('address');
+
+Route::redirect('/adres/{city?}/{street?}/{zipCode?}', '/address/{city?}/{street?}/{zipCode?}');
